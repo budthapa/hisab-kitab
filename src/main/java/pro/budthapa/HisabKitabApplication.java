0e82cb27e45ae88fff39f378d3c98600a0fd3ce6
Buddhi Bal Thapa <budthapa@gmail.com>
2017-03-09 10:50:44 +0400

    added navbar, messages.properties

--- src/main/java/pro/budthapa/HisabKitabApplication.java
+++ src/main/java/pro/budthapa/HisabKitabApplication.java
@@ -2,11 +2,22 @@ package pro.budthapa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class HisabKitabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HisabKitabApplication.class, args);
+	}
+	
+	@Bean
+	@Description("Spring message resolver")
+	public ResourceBundleMessageSource messageSource(){
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("i18n/messages");
+		return messageSource;
 	}
 }
